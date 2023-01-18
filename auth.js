7fce6dfe771af63bb2f90c7a811d4ece65c82236
smmartin1 <smarie333m@gmail.com>
2023-01-18 11:11:23 -0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -19,20 +19,19 @@ let generateJWTToken = (user) => {
 //Post Login
 module.exports = (router) => {
   router.post('/login', (req, res) => {
-    passport.authenticate('local', { session: false }, (error, user, info) => {
-      if (error || !user) {
+    passport.authenticate('local', { session: false }, (error, users, info) => {
+      if (error || !users) {
         return res.status(400).json({
-          message: 'Something is not right',
-          user: user
+          message: 'Something is wrong.',
+          user: users
         });
       }
-      req.login(user, { session: false }, (error) => {
+      req.login(users, { session: false }, (error) => {
         if (error) {
           res.send(error);
         }
-        
-        let token = generateJWTToken(user.toJSON());
-        return res.json({ user, token });
+        let token = generateJWTToken(users.toJSON());
+        return res.json({ users, token });
       });
     })(req, res);
   });
