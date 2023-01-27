cb017cfff915d1168c3dfd0f625fc641b769cd21
smmartin1 <smarie333m@gmail.com>
2023-01-27 10:36:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -108,10 +108,10 @@ app.get('/users', passport.authenticate('jwt', {session: false}), (req, res) => 
 //Get User by Username
 app.get('/users/:Username', passport.authenticate('jwt', {session: false}), (req, res) => {
     Users.findOne({ Username: req.params.Username }).then((users) => {
-        res.json(users);
+      res.json(users);
     }).catch((err) => {
-        console.error(err);
-        res.status.apply(500).send('Error: ' + err);
+      console.error(err);
+      res.status(500).send('Error: ' + err);
     });
 });
 
