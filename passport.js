c6ba052d5258838a7b3423979500142622c439fe
smmartin1 <smarie333m@gmail.com>
2023-05-08 09:35:16 -0400

    Correct spelling

--- passport.js
+++ passport.js
@@ -21,7 +21,7 @@ passport.use(new LocalStrategy({
     }
       
     if (!user) {
-      console.log('incorrect username');
+      console.log('Incorrect username');
       return callback(null, false, {message: 'Incorrect username'});
     }
 
